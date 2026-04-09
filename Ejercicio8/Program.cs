f71747aa268412d31be21c80ef8b5c82edb51294
Gerber Ramírez <koala57777@gmail.com>
2026-04-09 16:55:36 -0600

    Se terminó el proyecto 8.

--- Ejercicio8/Program.cs
+++ Ejercicio8/Program.cs
@@ -1,1 +1,51 @@
-﻿
+﻿int TArreglo;
+int ContadorImpares = 0;
+
+do
+{
+    Console.Write("Ingrese el tamaño del arreglo: ");
+
+    if (!int.TryParse(Console.ReadLine(), out TArreglo) || TArreglo <= 0)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Tamaño inválido");
+        TArreglo = -1;
+        Console.WriteLine();
+    }
+} while (TArreglo <= 0);
+Console.WriteLine();
+
+int[] numeros = new int[TArreglo];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    bool numeroValido = false;
+    do
+    {
+        Console.Write($"Ingrese el número {i + 1}: ");
+        if (!int.TryParse(Console.ReadLine(), out numeros[i]))
+        {
+            Console.WriteLine();
+            Console.WriteLine("Número inválido");
+            Console.WriteLine();
+        }
+        else
+        {
+            numeroValido = true;
+        }
+    } while (!numeroValido);
+    Console.WriteLine();
+}
+Console.WriteLine("Presione cualquier tecla para mostrar el resultado...");
+Console.ReadKey();
+Console.Clear();
+
+for (int j = 0; j < numeros.Length; j++)
+{
+    if (numeros[j] % 2 != 0)
+    {
+        ContadorImpares++;
+    }
+}
+
+Console.WriteLine($"Hay {ContadorImpares} impares en la lista");
