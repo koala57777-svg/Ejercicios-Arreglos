9df62e239dc6033a5944c8dd093ddded72d75e94
Gerber Ramírez <koala57777@gmail.com>
2026-04-09 16:31:28 -0600

    Se terminó el proyecto 2.

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿int TArreglo;
+
+do
+{
+    Console.Write("Ingrese el tamaño del arreglo: ");
+
+    if (!int.TryParse(Console.ReadLine(), out TArreglo) || TArreglo <= 0)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Tamaño inválido");
+        TArreglo = -1;
+        Console.WriteLine();
+    }
+} while (TArreglo <= 0);
+Console.WriteLine();
+
+double[] numeros = new double[TArreglo];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    bool numeroValido = false;
+    do
+    {
+        Console.Write($"Ingrese el número {i + 1}: ");
+        if (!double.TryParse(Console.ReadLine(), out numeros[i]))
+        {
+            Console.WriteLine();
+            Console.WriteLine("Número inválido");
+            Console.WriteLine();
+        }
+        else
+        {
+            numeroValido = true;
+        }
+    } while (!numeroValido);
+    Console.WriteLine();
+}
+Console.WriteLine("Presione cualquier tecla para mostrar el resultado...");
+Console.ReadKey();
+Console.Clear();
+
+for (int j = numeros.Length - 1; j >= 0; j--)
+{
+    Console.WriteLine($"Número {j + 1}: {numeros[j]}");
+    Console.WriteLine();
+}
