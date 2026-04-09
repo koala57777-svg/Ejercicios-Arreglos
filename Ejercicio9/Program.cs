5f83d4f03c0e139230d2f7b6c87357a2e32b7861
Gerber Ramírez <koala57777@gmail.com>
2026-04-09 17:10:37 -0600

    Se terminó el proyecto 9.

--- Ejercicio9/Program.cs
+++ Ejercicio9/Program.cs
@@ -1,1 +1,76 @@
-﻿
+﻿int TArreglo;
+int ContadorImpares = 0;
+
+do
+{
+    Console.Write("Ingrese el tamaño del arreglo: ");
+
+    if (!int.TryParse(Console.ReadLine(), out TArreglo) || TArreglo <= 0)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Tamaño inválido");
+        TArreglo = -1;
+        Console.WriteLine();
+    }
+} while (TArreglo <= 0);
+Console.WriteLine();
+
+double[] numeros = new double[TArreglo];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    bool numeroValido = false;
+    do
+    {
+        Console.Write($"Ingrese el número {i + 1}: ");
+        if (!double.TryParse(Console.ReadLine(), out numeros[i]))
+        {
+            Console.WriteLine();
+            Console.WriteLine("Número inválido");
+            Console.WriteLine();
+        }
+        else
+        {
+            numeroValido = true;
+        }
+    } while (!numeroValido);
+    Console.WriteLine();
+}
+Console.WriteLine("Presione cualquier tecla para mostrar el resultado...");
+Console.ReadKey();
+Console.Clear();
+
+double NumeroBuscar;
+bool BuscarValida = false;
+
+do
+{
+    BuscarValida = false;
+
+    Console.Write("Ingrese el número a buscar: ");
+
+    if (!double.TryParse(Console.ReadLine(), out NumeroBuscar))
+    {
+        Console.WriteLine();
+        Console.WriteLine("Tamaño inválido");
+        Console.WriteLine();
+    }
+    else
+    {
+        BuscarValida = true;
+    }
+} while (!BuscarValida);
+Console.WriteLine();
+
+for (int j=0; j< numeros.Length; j++)
+{
+    if (numeros[j] == NumeroBuscar)
+    {
+        Console.WriteLine($"Se encontró el número {NumeroBuscar}. En la posición {j+1} de la lista");
+        break;
+    }
+    else if (j == numeros.Length - 1)
+    {
+        Console.WriteLine($"No se encontró el número {NumeroBuscar} en la lista.");
+    }
+}
