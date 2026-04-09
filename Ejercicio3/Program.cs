a451d6f9a1448d59e076cfa317a9e1ba1bd060bf
Gerber Ramírez <koala57777@gmail.com>
2026-04-09 16:35:58 -0600

    Se terminó el proyecto 3.

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿int TArreglo;
+int SumaNumeros = 0;
+do
+{
+    Console.Write("Ingrese el tamaño del arreglo: ");
+
+    if (!int.TryParse(Console.ReadLine(), out TArreglo) || TArreglo <= 0)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Tamaño inválido");
+        TArreglo = -1;
+        Console.WriteLine();
+    }
+} while (TArreglo <= 0);
+Console.WriteLine();
+
+int[] numeros = new int[TArreglo];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    bool numeroValido = false;
+    do
+    {
+        Console.Write($"Ingrese el número {i + 1}: ");
+        if (!int.TryParse(Console.ReadLine(), out numeros[i]))
+        {
+            Console.WriteLine();
+            Console.WriteLine("Número inválido");
+            Console.WriteLine();
+        }
+        else
+        {
+            numeroValido = true;
+        }
+    } while (!numeroValido);
+    Console.WriteLine();
+}
+Console.WriteLine("Presione cualquier tecla para mostrar el resultado...");
+Console.ReadKey();
+Console.Clear();
+
+for (int j=0; j < numeros.Length; j++)
+{
+    SumaNumeros += numeros[j];
+}
+
+Console.WriteLine($"La suma de los números es: {SumaNumeros}");
