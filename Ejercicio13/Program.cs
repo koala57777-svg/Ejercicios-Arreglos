3b9848b8dfc251814dae3f918b0c0b08fc64aea8
Gerber Ramírez <koala57777@gmail.com>
2026-04-09 18:16:28 -0600

    Se terminó el proyecto 13.

--- Ejercicio13/Program.cs
+++ Ejercicio13/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿int TArreglo;
+
+do
+{
+    Console.Write("Ingrese el tamaño del arreglo: ");
+
+    if (!int.TryParse(Console.ReadLine(), out TArreglo) || TArreglo <= 0)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Tamaño inválido");
+        TArreglo = -1;
+        Console.WriteLine();
+    }
+} while (TArreglo <= 0);
+Console.WriteLine();
+
+double[] numeros = new double[TArreglo];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    bool numeroValido = false;
+    do
+    {
+        Console.Write($"Ingrese el número {i + 1}: ");
+        if (!double.TryParse(Console.ReadLine(), out numeros[i]))
+        {
+            Console.WriteLine();
+            Console.WriteLine("Número inválido");
+            Console.WriteLine();
+        }
+        else
+        {
+            numeroValido = true;
+        }
+    } while (!numeroValido);
+    Console.WriteLine();
+}
+Console.WriteLine("Presione cualquier tecla para mostrar el resultado...");
+Console.ReadKey();
+Console.Clear();
+
+double[] Copianumeros= new double[TArreglo];
+
+for (int j=0; j < numeros.Length; j++)
+{
+    Copianumeros[j]= numeros[j];
+}
+
+Console.WriteLine("Copia de la lista");
+Console.WriteLine();
+for (int j = 0; j < numeros.Length; j++)
+{
+    Console.WriteLine($"Número {j + 1}: {Copianumeros[j]}");
+    Console.WriteLine();
+}
